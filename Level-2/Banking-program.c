1700157f670ad51c92542ee2c494789fee969a88
Ujjwal-Jain-sys <ujjwaljain1234i@gmail.com>
2026-09-04 09:36:10 +0000

    Completed Banking program

--- Level-2/Banking-program.c
+++ Level-2/Banking-program.c
@@ -4,65 +4,92 @@
 #include <unistd.h>
 #include <time.h>
 
-int initial(){
-  int opt = 0;
-  printf("\nThis is a banking system\nSelect an option:\n1. Check Balance\n2. Deposit Money\n3. Withdraw Money\n4. Exit\n\nEnter your choice: ");
-  scanf("%i",&opt);
-  while (opt <1 || opt >4)
-  {
-    printf("Invalid Option, Plz Enter again: ");
-    scanf("%i",&opt);
-  }
-  return opt;
-}
-
-void checkBalance(double Amount){
-  printf("\nYour current balance is $%.2f\n",Amount);
-}
-
-double Deposit(double Amount){
-  double dep = 0.0;
-  bool exitcode = false;
-  do{
-    printf("\nEnter the amount you want to deposit: ");
-    scanf("%lf",&dep);
-    if(dep>=0){
-      printf("\nSuccessfully Deposited\n");
-      exitcode = true;
-    }
-    else{
-      printf("\nInvalid Amount to deposit\nReturning to the Main Menu");
-      dep = 0;
-      exitcode = true;
-    }
-  }while(exitcode == false);
-  return dep;
-  dep = 0;
-}
+void checkBalance(float balance);
+float deposit();
+float withdraw(float balance);
 
 int main(void) {
+  //banking Program
 
-  bool ExitCode = false;
+  int choice = 0;
+  float balance = 0;
+  
+  printf("*** Welcome To The Bank ***");
+
   do{
-    printf("-------------------------------------------------------------");
-    int option = initial();
-    switch(option)
-    {
-    double Amount = 0.0;
-    case 1:
-      checkBalance(Amount);
-      fflush(stdout);
-      usleep(500*1000);
-      break;
-    case 2:
-      Amount += Deposit(Amount);
-      fflush(stdout);
-      usleep(500*1000);
-      break;
-    default:
-      printf("\nAn error has occued while fetching Data\nReturning to Main Menu\n");
-      break;
+    printf("\nSelect an option:\n");
+    printf("1. Check Balance\n");
+    printf("2. Deposit Money\n");
+    printf("3. Withdraw Money\n");
+    printf("4. Exit\n");
+    printf("\nEnter your choice: ");
+    scanf("%d",&choice);
+
+    switch(choice){
+      case 1:
+        checkBalance(balance);
+        break;
+      case 2:
+        balance += deposit();
+        break;
+      case 3:
+        balance -= withdraw(balance);
+        break;
+      case 4:
+        printf("\nThank you for using the bank\n");
+        break;
+      default:
+        printf("\nInvalid Response, please try again\n");
+        break;
     }
-  }while(ExitCode == false);
+  }while(choice != 4);
   return 0;
+}
+
+void checkBalance(float balance){
+  printf("\nYour current Balance is ₹%.2f\n",balance);
+}
+
+float deposit(){
+  float amount = 0.0f;
+  printf("\nEnter the amount you want to deposit: ₹");
+  scanf("%f",&amount);
+
+  if(amount < 0){
+    for (int i = 5; i > 0; i--)
+    {
+      printf("\nInvalid Amount,Returning to the Main menu in %i\n",i);
+      sleep(1);
+    }
+    return 0.0f;
+  }
+  else{
+    printf("Successfully deposited ₹%.2f\n",amount);
+  }
+  return amount;
+}
+
+float withdraw(float balance){
+  float amount = 0.0f;
+  printf("\nYour Current Balance is ₹%.2f\n",balance);
+  printf("\nEnter the amount you want to withdraw: ");
+  scanf("%f",&amount);
+
+  if(amount < 0){
+    for (int i = 5; i > 0; i--)
+    {
+      printf("\nInvalid Amount,Returning to the Main menu in %i\n",i);
+      sleep(1);
+    }
+    return 0.0f;
+  }
+  else if(amount > balance){
+    printf("\nInsuffient funds, Can't withdraw this much\n");
+    return 0.0f;
+  }
+  else{
+    printf("\nSuccesfully Withdrawed ₹%.2f\n",amount);
+    printf("\nYour new balance is ₹%.2f",(balance-amount));
+    return amount;
+  }
 }
