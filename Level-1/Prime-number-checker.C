d8b4fe3a9f24b15dc8942c8c33235183ed676226
Ujjwal-Jain-sys <ujjwaljain1234i@gmail.com>
2026-09-09 16:42:01 +0000

    Modified:Prime checker completed and btw tomorrow is my first class in clg

--- Level-1/Prime-number-checker.C
+++ Level-1/Prime-number-checker.C
@@ -1,35 +1,43 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int primeCheck(int num);
 
 int main(void) {
-
-  int num = -1;// so even if user inputs string, it will show Invalid input
-  printf("\n*** Prime Number Checker ***\n\n");
-  printf("Enter the number to check: ");
+  int num = 0;
+  printf("\n*** Prime Number ***\n");
+  printf("\nEnter a number to check: ");
   scanf("%d",&num);
-  if(num>=0){
-    switch (num)
-    {
-    case 0:
-      printf("\nZero is not a prime number\n");
-      printf("As it has infinitely many solutions");
-      break;
-    case 1:
-      printf("\nOne is not a prime number\n");
-      printf("As it has only one factor, itself\n");
-      break;
-    case 2:
-      printf("\nTwo is a prime number\n");
-      break;
-    case 3:
-      printf("\nThree is a prime number\n");
-      break;
-    default:
-      int k = (num-1)/6;
+  if(num < 0){
+    printf("\nNegative integers are not prime numbers, you dumb creature\n");
+  }
+  else{
+    if(primeCheck(num)){
+      printf("\n%d is a prime number\n",num);
+    }
+    else{
+      printf("\n%d is a composite number\n",num);
+    }
+  }
+  return 0;
+}
+
+int primeCheck(int num){
+  if(num <= 1){//checks if less than 1
+    return false;
+  }
+  if(num <= 3 && num > 1){//checks if number is 2 or 3
+    return true;
+  }
+  if(num % 2 == 0 || num % 3 == 0){//checks if number is multiple of 2 or 3
+    return false;
+  }// num can be 5,7,11,13,17,19,23,25 ...
+  
+  for(long int i = 5;i*i <= num;i += 6){// here, i is 5 and then 11 and i + 2 is 13 which checks if the number is divisible by any of the prime number
+    if(num % i == 0 || num % (i+2) == 0){
+      return false;
       break;
     }
   }
-  else{
-    printf("\nInvalid Input,Try again\n");
-  }
-  return 0;
+  return true;
 }
